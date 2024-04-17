4bcbadce68f6ef16bbdd70380198d67ee6f056f7
bmacer <bmacer@cisco.com>
2024-04-17 11:45:22 -0400

    framework

---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+const Nav = () => {
+  return (
+    <nav>
+      <img src="/Logo.svg" alt="Little Lemon Logo" />
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
